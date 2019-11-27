cc4e0760b5743ae9d85a0b19b4eebc9d2c0b4b33
jihye <zehye.01@gmail.com>
2019-11-27 11:47:46 +0900

    codedeploy를 위한 buildspec.yml과 Dockerfile 생성

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -35,10 +35,6 @@ def get_env_variable(var_name, default=None):
         return default
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
